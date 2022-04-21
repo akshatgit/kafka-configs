a3b0364717f5b17a359218a730df2c096e1c1e2f
akshatgit <akshat14132@iiitd.ac.in>
2022-04-21 14:18:27 -0500

    fix script and more configs

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt-get install -y openjdk-11-jre
+sudo apt install -y  maven
+
 sudo useradd zk -m
 sudo usermod --shell /bin/bash zk
- echo "zk:1234" | sudo chpasswd
- sudo usermod -aG sudo zk
+echo "zk:1234" | sudo chpasswd
+sudo usermod -aG sudo zk
 
 sudo mkdir -p /data/
 sudo mkfs.ext4 /dev/sdb
@@ -11,9 +15,14 @@ sudo mount /dev/sdb /data
 sudo mkdir -p /data/zookeeper
 sudo chown -R zk /data/zookeeper
 sudo mkdir -p /data/kafka
+
+sudo cp zookeeper/zk.service /etc/systemd/system/zk.service
+
 cd /opt
 sudo wget https://dlcdn.apache.org/zookeeper/zookeeper-3.8.0/apache-zookeeper-3.8.0-bin.tar.gz
 sudo tar -xvf apache-zookeeper-3.8.0-bin.tar.gz
-sudo chown zk -R  apache-zookeeper-3.8.0-bin.tar.gz
-sudo ln -s apache-zookeeper-3.8.0-bin.tar.gz zookeeper
-sudo chown -h zk zookeeper
+sudo chown zk -R  apache-zookeeper-3.8.0-bin
+sudo ln -s apache-zookeeper-3.8.0-bin zookeeper
+sudo chown -R zk zookeeper
+
+sudo -u zk cp /users/hsagar/kafka-configs/zookeeper/zoo.cfg .
